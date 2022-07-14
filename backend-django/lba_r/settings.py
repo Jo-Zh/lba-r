7ab2785a4b56414e0437d0b314e43df85ae608f1
Jo-Zh <joannaweinazh@gmail.com>
2022-07-14 16:49:14 +1000

    stage

--- backend-django/lba_r/settings.py
+++ backend-django/lba_r/settings.py
@@ -28,7 +28,7 @@ with open('/etc/secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
