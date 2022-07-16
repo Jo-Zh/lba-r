3039fde24c39dae95a28b0a7a85cec058be82468
Jo-Zh <joannaweinazh@gmail.com>
2022-07-16 22:06:18 +1000

    style upate

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import Article from "./component/Article";
 import Search from "./component/Search";
 import Category from "./component/Category";
+import "./home.css";
 
 const Home = ({ posts }: HomeProps) => {
   return (
     <div>
-      <div className="row text-center py-3">
+      <div className="overlay row text-center py-3">
         <h1>Explore and Sharing learning experience here... </h1>
       </div>
       <div className="d-flex flex-spacebetween bg-light">
@@ -14,23 +15,26 @@ const Home = ({ posts }: HomeProps) => {
         <Category />
       </div>
 
-      <div className="container-fluid px-4 my-4">
-        <div className="row g-4 ">
-          {posts.map((post) => {
-            return (
-              <Article
-                key={post.id}
-                postcategory={post.category}
-                postimage={post.cover}
-                posttitle={post.title}
-                postcreater={post.creater}
-                postdate={post.date}
-                postcontent={post.content}
-                id={post.id}
-              />
-            );
-          })}
-          <div className="col">No results</div>
+      <div className="container-fluid ">
+        <div className="row responsive-margin">
+          {posts.length !== 0 ? (
+            posts.map((post) => {
+              return (
+                <Article
+                  key={post.id}
+                  postcategory={post.category}
+                  postimage={post.cover}
+                  posttitle={post.title}
+                  postcreater={post.creater}
+                  postdate={post.date}
+                  postcontent={post.content}
+                  id={post.id}
+                />
+              );
+            })
+          ) : (
+            <div className="row">No results</div>
+          )}
         </div>
       </div>
     </div>
--- src/routes/component/Article.tsx
+++ src/routes/component/Article.tsx
@@ -16,34 +16,32 @@ const Article = ({
   postimage,
 }: ArticleProps) => {
   return (
-    <div id="test-article" className="col-md-4">
-      <div className="card box-shadow d-flex flex-row p-0 align-items-center h-100">
-        <div className=" card-body d-flex flex-column align-items-start m-0 h-100">
-          <p className="bi bi-tag">{postcategory}</p>
-          <blockquote className="blockquote mb-0">
-            <p className="fw-bold lead fs-4">
-              <a href="{%url 'detail' post.id %}"></a>
-              {posttitle}
-            </p>
-            <footer className="blockquote-footer">
-              {postdate.toString()} by {postcreater}
-            </footer>
-            <p className="lh-sm lead">{truncate(postcontent)}</p>
-          </blockquote>
-          <Link className="card-link" to={`post/${id}`} key={id}>
-            Read more...
-          </Link>
-        </div>
-        <div className="d-flex me-4 align-itecarms-center">
-          <Figure>
-            <Figure.Image
-              width={200}
-              height={250}
-              alt="Thumbnail [200x250]"
-              src={postimage}
-            />
-          </Figure>
-        </div>
+    <div className="col card  box-shadow d-flex flex-row align-items-center border border-light border-3 bg-transparent">
+      <div className=" card-body d-flex flex-column align-items-start m-0 h-100">
+        <p className="bi bi-tag">{postcategory}</p>
+        <blockquote className="blockquote mb-0">
+          <p className="fw-bold lead fs-4">
+            <a href="{%url 'detail' post.id %}"></a>
+            {posttitle}
+          </p>
+          <footer className="blockquote-footer">
+            {postdate.toString()} by {postcreater}
+          </footer>
+          <p className="lh-sm lead">{truncate(postcontent)}</p>
+        </blockquote>
+        <Link className="card-link" to={`post/${id}`} key={id}>
+          Read more...
+        </Link>
+      </div>
+      <div className="d-flex me-4 align-itecarms-center">
+        <Figure>
+          <Figure.Image
+            width={200}
+            height={250}
+            alt="Thumbnail [200x250]"
+            src={postimage}
+          />
+        </Figure>
       </div>
     </div>
   );
